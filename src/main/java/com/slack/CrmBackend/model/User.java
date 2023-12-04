18a606186ed7d792728a4580437cc95c1e73ffb9
David SALAVESSA <david.salavessa@gmail.com>
2023-12-04 15:51:08 +0100

    Add EAGER instead LAZY to entities

--- src/main/java/com/slack/CrmBackend/model/User.java
+++ src/main/java/com/slack/CrmBackend/model/User.java
@@ -7,6 +7,7 @@ import java.util.List;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -41,7 +42,7 @@ public class User {
   private LocalDateTime updatedAt;
 
   /** User messages */
-  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
   private List<Message> messages = new ArrayList<Message>();
 
   public User() {
