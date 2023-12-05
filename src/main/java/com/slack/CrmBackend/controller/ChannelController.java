9634b3d84a43a4d154522731c8df7bc0790f4189
MateoBrunetti <mateo.brunetti42@orange.fr>
2023-12-05 15:42:40 +0100

    Comments added for Controllers ChannelController, MessageController and UserController

--- src/main/java/com/slack/CrmBackend/controller/ChannelController.java
+++ src/main/java/com/slack/CrmBackend/controller/ChannelController.java
@@ -9,29 +9,59 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Controller for Channel Entities, manages REST API
+ * Utilizes Spring annotations for request mappings and dependency injection.
+ * Provides CRUD operations : get all, get one, adding, updating, and deleting channels.
+ */
 @RestController
 @RequestMapping("channel")
 public class ChannelController {
 
+    /**
+     * Autowiring the ChannelService for handling business logic related to messages.
+     */
     @Autowired
     ChannelService channelService;
 
+    /**
+     * Endpoint to retrieve all channels.
+     * @return List<Channels>
+     */
     @GetMapping
     public List<Channel> getAllChannels() {
         return channelService.getAllChannels();
     }
 
+    /**
+     * Endpoint to retrieve a chanel by its ID.
+     * Use Optional to handle the possibility of a null result.
+     * Return a response with the channel if it exists.
+     * Return a not found response if the channel doesn't exist.
+     * @param Integer id
+     * @return ResponseEntity(Channel channel)
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Channel> getChannelById(@PathVariable("id") Integer id){
+
         Optional<Channel> optional = channelService.getChannelById(id);
+
         if(optional.isPresent()){
             Channel channel = optional.get();
             return ResponseEntity.ok(channel);
+
         } else {
             return ResponseEntity.notFound().build();
         }
     }
 
+    /**
+     * Endpoint to add a new channel.
+     * Call the service to create a new channel.
+     * Return a response with the created channel.
+     * @param Channel newChannel
+     * @return ResponseEntity(Channel newChannel)
+     */
     @PostMapping
     @ResponseStatus( HttpStatus.CREATED )
     public ResponseEntity<Channel> addChannel(@RequestBody Channel newChannel){
@@ -39,26 +69,49 @@ public class ChannelController {
         return ResponseEntity.ok(newChannel);
     }
 
+    /**
+     * Endpoint to update an existing channel.
+     * Check if the channel with the given ID exists.
+     * Set the ID and call the service to update the channel.
+     * Return a response with the updated channel.
+     * Return a not found response if the channel doesn't exist.
+     * @param Integer id
+     * @param Channel channel
+     * @return ResponseEntity
+     */
     @PutMapping("/{id}")
     public ResponseEntity<Channel> putChannel(@PathVariable Integer id, @RequestBody Channel channel) {
+
         Optional<Channel> existingChannel = channelService.getChannelById(id);
 
         if (existingChannel.isPresent()) {
             channel.setId(id);
             Channel updatedChannel = channelService.updateChannel(id, channel);
+
             return ResponseEntity.ok(updatedChannel);
         } else {
             return ResponseEntity.notFound().build();
         }
     }
 
+    /**
+     * Endpoint to delete a channel by its ID.
+     * Check if the channel with the given ID exists.
+     * Return a not found response if the channel doesn't exist.
+     * Call the service to delete the message.
+     * Return a response indicating successful deletion.
+     * @param Integer id
+     * @return ResponseEntity
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity deleteChannel(@PathVariable("id") Integer id){
+
         Optional<Channel> optional = channelService.getChannelById(id);
+
         if(optional.isEmpty()){
             return ResponseEntity.notFound().build();
-        }
-        else {
+
+        } else {
             channelService.deleteChannel(id);
             return ResponseEntity.ok().build();
         }
--- src/main/java/com/slack/CrmBackend/controller/MessageController.java
+++ src/main/java/com/slack/CrmBackend/controller/MessageController.java
@@ -9,18 +9,38 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Controller for Message Entities, manages REST API
+ * Utilizes Spring annotations for request mappings and dependency injection.
+ * Provides CRUD operations: get all, get one, adding, updating, and deleting messages.
+ */
 @RestController
 @RequestMapping("message")
 public class MessageController {
 
+    /**
+     * Autowiring the MessageService for handling business logic related to messages.
+     */
     @Autowired
     MessageService messageService;
 
+    /**
+     * Endpoint to retrieve all messages.
+     * @return List<Messages>
+     */
     @GetMapping
     public List<Message> getAllMessages() {
         return messageService.getAllMessages();
     }
 
+    /**
+     * Endpoint to retrieve a message by its ID.
+     * Use Optional to handle the possibility of a null result.
+     * Return a response with the message if it exists.
+     * Return a not found response if the message doesn't exist.
+     * @param Integer id
+     * @return ResponseEntity(Message message)
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Message> getMessageById(@PathVariable("id") Integer id){
         Optional<Message> optional = messageService.getMessageById(id);
@@ -32,6 +52,13 @@ public class MessageController {
         }
     }
 
+    /**
+     * Endpoint to add a new message.
+     * Call the service to create a new message.
+     * Return a response with the created message.
+     * @param Message newMessage
+     * @return ResponseEntity(Message newMessage)
+     */
     @PostMapping
     @ResponseStatus( HttpStatus.CREATED )
     public ResponseEntity<Message> addMessage(@RequestBody Message newMessage){
@@ -39,6 +66,16 @@ public class MessageController {
         return ResponseEntity.ok(newMessage);
     }
 
+    /**
+     * Endpoint to update an existing message.
+     * Check if the message with the given ID exists.
+     * Set the ID and call the service to update the message.
+     * Return a response with the updated message.
+     * Return a not found response if the message doesn't exist.
+     * @param Integer id
+     * @param Message message
+     * @return ResponseEntity(Message updatedMessage)
+     */
     @PutMapping("/{id}")
     public ResponseEntity<Message> putMessage(@PathVariable Integer id, @RequestBody Message message) {
         Optional<Message> existingMessage = messageService.getMessageById(id);
@@ -52,6 +89,15 @@ public class MessageController {
         }
     }
 
+    /**
+     * Endpoint to delete a message by its ID.
+     * Check if the message with the given ID exists.
+     * Return a not found response if the message doesn't exist.
+     * Call the service to delete the message.
+     * Return a response indicating successful deletion.
+     * @param Integer id
+     * @return ResponseEntity
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity deleteMessage(@PathVariable("id") Integer id){
         Optional<Message> optional = messageService.getMessageById(id);
