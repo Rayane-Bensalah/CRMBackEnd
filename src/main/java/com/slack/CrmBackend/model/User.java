ad54d0a6ea0b51bada6beff8efdfc1b8c35d1fb0
David SALAVESSA <david.salavessa@gmail.com>
2023-12-06 09:55:52 +0100

    Fix FetchType error

--- src/main/java/com/slack/CrmBackend/model/User.java
+++ src/main/java/com/slack/CrmBackend/model/User.java
@@ -4,11 +4,12 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
